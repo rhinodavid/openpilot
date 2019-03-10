2513e204131e3018433448894a1200c4b1619c38
David Walsh <rhinodavid@github.com>
2019-03-10 13:52:25 -0600

    Up the near follow time gap

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -137,8 +137,8 @@ class LongitudinalMpc(object):
   # https://github.com/rhinodavid/CommaButtons
   DEFAULT_FOLLOW_TIME = 1.8
   TIME_GAP_VALUES = {
-    'near': 1.0,
-    'medium': 1.4,
+    'near': 1.2,
+    'medium': 1.5,
     'far': 1.8,
     'unknown': DEFAULT_FOLLOW_TIME
   }
@@ -237,9 +237,6 @@ class LongitudinalMpc(object):
     # https://github.com/rhinodavid/CommaButtons
     set_time_gap = CS.carState.timeGap
     
-    if int(t) % 2 == 0:
-      logging.debug("CS Time Gap\t%s" % CS.carState.timeGap)
-
     self.log_this_cycle = True if self.prev_time_gap != set_time_gap else False
 
     if self.log_this_cycle:
@@ -248,7 +245,7 @@ class LongitudinalMpc(object):
       logging.debug("prev follow time:\t%.2f" % prev_follow_time)
 
     follow_time = self.TIME_GAP_VALUES[set_time_gap] if set_time_gap in self.TIME_GAP_VALUES else self.DEFAULT_FOLLOW_TIME
-    n_its = self.libmpc.run_mpc(self.cur_state, self.mpc_solution, self.a_lead_tau, a_lead, 1.1)
+    n_its = self.libmpc.run_mpc(self.cur_state, self.mpc_solution, self.a_lead_tau, a_lead, follow_time)
     duration = int((sec_since_boot() - t) * 1e9)
 
     self.send_mpc_solution(n_its, duration)
